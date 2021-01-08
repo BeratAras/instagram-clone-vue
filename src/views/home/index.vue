8d368defe558f36b8db8a0b6c52755806a26a5df
Berat Aras <berat@webeyondcreative.com>
2021-01-08 12:10:13 +0300

    anasayfa layout yapısı oluşturuldu

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,70 @@
 <template>
   <div class="home">
-    <CustomText size="normal">Home</CustomText>
+    <div class="timeline">
+      <CustomText size="normal">Base</CustomText>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, sunt, vero! A alias aliquam aperiam
+        dignissimos doloribus, necessitatibus numquam omnis provident quasi quisquam repudiandae saepe, tempore velit
+        veritatis voluptas voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, sunt, vero! A alias aliquam aperiam
+        dignissimos doloribus, necessitatibus numquam omnis provident quasi quisquam repudiandae saepe, tempore velit
+        veritatis voluptas voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, sunt, vero! A alias aliquam aperiam
+        dignissimos doloribus, necessitatibus numquam omnis provident quasi quisquam repudiandae saepe, tempore velit
+        veritatis voluptas voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, sunt, vero! A alias aliquam aperiam
+        dignissimos doloribus, necessitatibus numquam omnis provident quasi quisquam repudiandae saepe, tempore velit
+        veritatis voluptas voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, sunt, vero! A alias aliquam aperiam
+        dignissimos doloribus, necessitatibus numquam omnis provident quasi quisquam repudiandae saepe, tempore velit
+        veritatis voluptas voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, sunt, vero! A alias aliquam aperiam
+        dignissimos doloribus, necessitatibus numquam omnis provident quasi quisquam repudiandae saepe, tempore velit
+        veritatis voluptas voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, sunt, vero! A alias aliquam aperiam
+        dignissimos doloribus, necessitatibus numquam omnis provident quasi quisquam repudiandae saepe, tempore velit
+        veritatis voluptas voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, sunt, vero! A alias aliquam aperiam
+        dignissimos doloribus, necessitatibus numquam omnis provident quasi quisquam repudiandae saepe, tempore velit
+        veritatis voluptas voluptatibus.</p>
+    </div>
+
+    <div class="sidebar">
+      <CustomText size="normal">Sidebar</CustomText>
+    </div>
   </div>
 </template>
 
 
-
-
 <script>
 import CustomText from "@/components/CustomText";
+
 export default {
   name: "Home",
-  components: { CustomText },
+  components: { CustomText }
 };
 </script>
 
 
-
 <style lang="scss" scoped>
+.home {
+  display: grid;
+  grid-template-columns: 1fr 295px;
+  grid-gap: 30px;
+  max-width: none;
+}
+
+@media only screen and (max-width: 980px) {
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .timeline{
+      max-width: 605px;
+    }
+  }
+  .sidebar {
+    display: none;
+  }
+}
 </style>
